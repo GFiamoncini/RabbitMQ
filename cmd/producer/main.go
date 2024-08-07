7cfc6bec11a32d055e399fbf448508919b50953f
GFiamoncini <biel.fiamoncini@gmail.com>
2024-08-06 22:48:43 -0300

    Versao-1

---
+++ cmd/producer/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"Eventos/pkg/rabbitmq"
+)
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	rabbitmq.Publish(ch, "Teste do GF", "amq.direct")
+}
